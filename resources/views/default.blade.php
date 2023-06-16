55545020a040e2340a0607404c1f8d02c7e707db
rahmat <cogantortorridho@gmail.com>
2023-06-16 18:42:37 +0700

    CLone

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -3,6 +3,6 @@
     <hr>
     <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
         <div class="bi-house-fill me-3 fs-1"></div>
-        <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
+        <h4 class="mb-0">Well done! this is rahmat {{ $pageTitle }}.</h4>
     </div>
 </div>
