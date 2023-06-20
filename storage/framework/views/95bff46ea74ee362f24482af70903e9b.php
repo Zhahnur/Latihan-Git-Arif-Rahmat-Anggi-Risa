bf5c28f0982a04f186cc998950af875e0b6fff77
Anggi Aulia Putri <anggiauputri@gmail.com>
2023-06-20 16:32:09 +0700

    Halaman Profile (Anggi)

---
+++ storage/framework/views/95bff46ea74ee362f24482af70903e9b.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arif Rahmat Anggi Risa</title>
+    <style>
+        body {
+            background-color: #222222;
+            color: #ffffff;
+            font-family: 'Arial', sans-serif;
+            padding: 30px;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        h1 {
+            font-size: 36px;
+            font-weight: bold;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+
+        .logo {
+            display: block;
+            margin: 0 auto;
+            width: 200px;
+            height: auto;
+            margin-bottom: 30px;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 12px 24px;
+            font-size: 18px;
+            text-align: center;
+            text-decoration: none;
+            background-color: #ff5500;
+            color: #ffffff;
+            border: none;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+
+        .btn:hover {
+            background-color: #cc4400;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Arif - Rahmat - Anggi - Risa (ARAR)</h1>
+        <img class="logo" src="<?php echo e(Vite::asset('resources/images/arar-logo.png')); ?>" alt="ARAR Logo">
+
+        <div style="text-align: center;">
+            <a class="btn" href="home">Halaman Utama</a>
+        </div>
+    </div>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+</html>
+<?php /**PATH C:\Users\USER\Latihan-Git-Arif-Rahmat-Anggi-Risa\resources\views/welcome.blade.php ENDPATH**/ ?>
--- storage/framework/views/d9ce5ef224b5319198a84b65498af32a.php
+++ storage/framework/views/d9ce5ef224b5319198a84b65498af32a.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile Kelompok</title>
-    @vite('resources/sass/app.scss')
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
 
     <style>
         .container {
@@ -29,41 +29,43 @@
       </style>
 </head>
 
-@extends('layouts.app')
 
-@section('content')
-@include('default')
+
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 <body>
     <div class="container">
         <div class="profile">
-          <img src="{{ Vite::asset('resources/images/profil1.jpeg') }}" alt="Foto Profil">
+          <img src="<?php echo e(Vite::asset('resources/images/profil1.jpeg')); ?>" alt="Foto Profil">
           <div>
             <b><p>Anggi Aulia Putri</p></b>
           </div>
         </div>
 
         <div class="profile">
-          <img src="{{ Vite::asset('resources/images/profil2.jpeg') }}" alt="Foto Profil">
+          <img src="<?php echo e(Vite::asset('resources/images/profil2.jpeg')); ?>" alt="Foto Profil">
           <div>
             <b><p>Dewi Nahrisa Amelia</p></b>
           </div>
         </div>
 
         <div class="profile">
-          <img src="{{ Vite::asset('resources/images/profil3.jpeg') }}" alt="Foto Profil">
+          <img src="<?php echo e(Vite::asset('resources/images/profil3.jpeg')); ?>" alt="Foto Profil">
           <div>
             <b><p>M. Zhahnur Arif</p></b>
           </div>
         </div>
 
         <div class="profile">
-            <img src="{{ Vite::asset('resources/images/profil4.jpeg') }}" alt="Foto Profil">
+            <img src="<?php echo e(Vite::asset('resources/images/profil4.jpeg')); ?>" alt="Foto Profil">
             <div>
                 <b><p>Ridho Rahmatulloh</p></b>
             </div>
           </div>
       </div>
 </body>
-@endsection
-    @vite('resources/js/app.js')
+<?php $__env->stopSection(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
 </html>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\USER\Latihan-Git-Arif-Rahmat-Anggi-Risa\resources\views/profile.blade.php ENDPATH**/ ?>
