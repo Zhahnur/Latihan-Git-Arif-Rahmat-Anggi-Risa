349385067c7d48c488530f3dba835d1db2b6ce78
Dewi Nahrisa Amelia <dewinahrisa.amel@gmail.com>
2023-06-20 16:28:30 +0700

    Halaman Group Members (Dewi Nahrisa)

--- storage/framework/views/cb2c8f26ceec723c64f4a74a03d4004d.php
+++ storage/framework/views/cb2c8f26ceec723c64f4a74a03d4004d.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Group Members</title>
-    @vite('resources/sass/app.scss')
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container">
-            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi bi-stars me-2"></i>ARAR</a>
+            <a href="<?php echo e(route('home')); ?>" class="navbar-brand mb-0 h1"><i class="bi bi-stars me-2"></i>ARAR</a>
 
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
@@ -20,13 +20,13 @@
                 <hr class="d-lg-none text-white-50">
 
                 <ul class="navbar-nav flex-row flex-wrap">
-                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
-                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}" class="nav-link active">Group Members</a></li>
+                    <li class="nav-item col-2 col-md-auto"><a href="<?php echo e(route('home')); ?>" class="nav-link">Home</a></li>
+                    <li class="nav-item col-2 col-md-auto"><a href="<?php echo e(route('employees.index')); ?>" class="nav-link active">Group Members</a></li>
                 </ul>
 
                 <hr class="d-lg-none text-white-50">
 
-                <a href="{{ route('profile') }}" class="btn btn-outline-light my-2 ms-md-auto"><i class="bi-person-circle me-1"></i> Members Profile</a>
+                <a href="<?php echo e(route('profile')); ?>" class="btn btn-outline-light my-2 ms-md-auto"><i class="bi-person-circle me-1"></i> Members Profile</a>
             </div>
         </div>
     </nav>
@@ -38,7 +38,7 @@
             </div>
             <div class="col-lg-3 col-xl-2">
                 <div class="d-grid gap-2">
-                    <a href="{{ route('employees.create') }}" class="btn btn-primary bg-dark">New Group Members</a>
+                    <a href="<?php echo e(route('employees.create')); ?>" class="btn btn-primary bg-dark">New Group Members</a>
                 </div>
             </div>
         </div>
@@ -62,13 +62,13 @@
                         <td>1204210075</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ route('employees.show', ['employee' => 1]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="{{ route('employees.edit', ['employee' => 1]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+                                <a href="<?php echo e(route('employees.show', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="<?php echo e(route('employees.edit', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                 <div>
-                                    <form action="{{ route('employees.destroy', ['employee' => 1]) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
+                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 1])); ?>" method="POST">
+                                        <?php echo csrf_field(); ?>
+                                        <?php echo method_field('delete'); ?>
                                         <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
                                     </form>
                                 </div>
@@ -82,13 +82,13 @@
                         <td>1204210107</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ route('employees.show', ['employee' => 2]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="{{ route('employees.edit', ['employee' => 2]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+                                <a href="<?php echo e(route('employees.show', ['employee' => 2])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="<?php echo e(route('employees.edit', ['employee' => 2])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                 <div>
-                                    <form action="{{ route('employees.destroy', ['employee' => 2]) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
+                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 2])); ?>" method="POST">
+                                        <?php echo csrf_field(); ?>
+                                        <?php echo method_field('delete'); ?>
                                         <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
                                     </form>
                                 </div>
@@ -102,13 +102,13 @@
                         <td>1204210002</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ route('employees.show', ['employee' => 3]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="{{ route('employees.edit', ['employee' => 3]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+                                <a href="<?php echo e(route('employees.show', ['employee' => 3])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="<?php echo e(route('employees.edit', ['employee' => 3])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                 <div>
-                                    <form action="{{ route('employees.destroy', ['employee' => 3]) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
+                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 3])); ?>" method="POST">
+                                        <?php echo csrf_field(); ?>
+                                        <?php echo method_field('delete'); ?>
                                         <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
                                     </form>
                                 </div>
@@ -122,13 +122,13 @@
                         <td>1204210111</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ route('employees.show', ['employee' => 1]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="{{ route('employees.edit', ['employee' => 1]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+                                <a href="<?php echo e(route('employees.show', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="<?php echo e(route('employees.edit', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                 <div>
-                                    <form action="{{ route('employees.destroy', ['employee' => 1]) }}" method="POST">
-                                        @csrf
-                                        @method('delete')
+                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 1])); ?>" method="POST">
+                                        <?php echo csrf_field(); ?>
+                                        <?php echo method_field('delete'); ?>
                                         <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
                                     </form>
                                 </div>
@@ -140,6 +140,7 @@
         </div>
     </div>
 
-    @vite('resources/js/app.js')
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
 </body>
 </html>
+<?php /**PATH /Users/dewinahrisaamelia/Latihan-Git-Arif-Rahmat-Anggi-Risa/resources/views/employee/index.blade.php ENDPATH**/ ?>
